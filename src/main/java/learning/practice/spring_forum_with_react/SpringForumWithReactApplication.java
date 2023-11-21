04d73190431f54f323cb416b46f4c8b598a99d1c
kgpksh <tkdgns543@gmail.com>
2023-11-21 23:34:30 +0900

    feat : MariaDB 관련 설정

--- src/main/java/learning/practice/spring_forum_with_react/SpringForumWithReactApplication.java
+++ src/main/java/learning/practice/spring_forum_with_react/SpringForumWithReactApplication.java
@@ -2,8 +2,10 @@ package learning.practice.spring_forum_with_react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringForumWithReactApplication {
 
 	public static void main(String[] args) {

365fcdb2a0c3c91837b4c1f6760db9a7416a6119
kgpksh <tkdgns543@gmail.com>
2023-11-22 20:53:49 +0900

    feat: 로그인 되지 않은 사용자만 회원가입이나 로그인 접근 가능하게 제한

--- src/main/java/learning/practice/spring_forum_with_react/base/security/SecureConfig.java
+++ src/main/java/learning/practice/spring_forum_with_react/base/security/SecureConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,9 +16,16 @@ public class SecureConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.
                 authorizeHttpRequests(request -> {
-                    request.requestMatchers(new AntPathRequestMatcher("/**")).permitAll();
+                    request.requestMatchers(
+                            new AntPathRequestMatcher("/member/signup")
+                            ,new AntPathRequestMatcher("/member/login")).anonymous();
                     request.anyRequest().authenticated();
                 }).
                 build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

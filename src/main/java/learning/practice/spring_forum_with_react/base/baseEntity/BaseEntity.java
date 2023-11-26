6b31e59794842039170d7773ba1f4cba56743283
kgpksh <tkdgns543@gmail.com>
2023-11-27 08:00:16 +0900

    refactor: 엔티티의 아이디와 생성 및 수정 날짜 생성 컬럼 부분을 공통 BaseEntity로 분리

---
+++ src/main/java/learning/practice/spring_forum_with_react/base/baseEntity/BaseEntity.java
@@ -0,0 +1,23 @@
+package learning.practice.spring_forum_with_react.base.baseEntity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Entity
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date lastModifiedDate;
+}
--- src/main/java/learning/practice/spring_forum_with_react/boundedContext/member/entity/Member.java
+++ src/main/java/learning/practice/spring_forum_with_react/boundedContext/member/entity/Member.java
@@ -1,37 +1,25 @@
 package learning.practice.spring_forum_with_react.boundedContext.member.entity;
 
 import jakarta.persistence.*;
+import learning.practice.spring_forum_with_react.base.baseEntity.BaseEntity;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
 @Data
-@EntityListeners(AuditingEntityListener.class)
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
 
-    @CreatedDate
-    private Date createdDate;
-    @LastModifiedDate
-    private Date lastModifiedDate;
-
+public class Member extends BaseEntity {
     private String username;
     private String password;
 
     private boolean isAdmin;
     private String providerTypeCode;
 
-    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+    public List<? extends GrantedAuthority> takeGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
 

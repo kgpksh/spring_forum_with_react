c36d20333d2b5478fb4d10994e512d044cabb313
kgpksh <tkdgns543@gmail.com>
2023-12-09 15:12:29 +0900

    feat: 대용량 데이터 삽입 기능 구현 해당 테스트는 작동 범위에서 제외 profile을 통해 대용량 DB 분리

---
+++ src/main/java/learning/practice/spring_forum_with_react/base/testData/BulkDataManager.java
@@ -0,0 +1,38 @@
+package learning.practice.spring_forum_with_react.base.testData;
+
+import learning.practice.spring_forum_with_react.boundedContext.post.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BulkDataManager {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void saveBulk(List<Post> postList, int size) {
+        String sql = """
+                INSERT INTO post (created_date, last_modified_date, subject, author, title, content) VALUES (NOW(), NOW(), ?, ?, ?, ?)
+                """;
+
+        int batchSize = 20000;
+        for (int i = 0; i < size; i+= batchSize) {
+            List<Post> batchList = postList.subList(i, Math.min(i + batchSize, size));
+
+            jdbcTemplate.batchUpdate(sql,
+                    batchList,
+                    size,
+                    (PreparedStatement ps, Post post) -> {
+                        ps.setString(1, post.getSubject());
+                        ps.setString(2, post.getAuthor());
+                        ps.setString(3, post.getTitle());
+                        ps.setString(4, post.getContent());
+                    });
+        }
+    }
+}
---
+++ src/test/java/learning/practice/spring_forum_with_react/base/testData/BulkDataManagerTest.java
@@ -0,0 +1,67 @@
+package learning.practice.spring_forum_with_react.base.testData;
+
+import learning.practice.spring_forum_with_react.boundedContext.post.entity.Post;
+import learning.practice.spring_forum_with_react.boundedContext.post.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Tag("bulkDbTest")
+@ActiveProfiles("highLoad")
+@SpringBootTest
+class BulkDataManagerTest {
+    @Autowired
+    private BulkDataManager bulkDataManager;
+    @Autowired
+    private PostRepository postRepository;
+
+    private List<Post> createData(int size) {
+        String[] subjects = {"Cook", "Book", "Coding", "Humor"};
+        String author = "author";
+        String title = "title";
+        String content = "content";
+
+        List<Post> sampleData = new ArrayList<>(size);
+        for (int i = 1; i < size + 1; i++) {
+            Post post = new Post();
+            post.setAuthor(author + i % 50);
+            post.setSubject(subjects[i % 4]);
+            post.setTitle(title + i);
+            post.setContent(content + i);
+            sampleData.add(post);
+        }
+
+        return sampleData;
+    }
+    @Test
+    @DisplayName("jdbc 배치 단건 입력")
+    void insertSingle() {
+        int size = 1;
+        List<Post> sampleData = createData(size);
+        bulkDataManager.saveBulk(sampleData, size);
+
+        Optional<Post> selected = postRepository.findById(1L);
+        assertThat(selected.isPresent()).isTrue();
+        assertThat(selected.get().getAuthor()).isEqualTo("author1");
+    }
+
+    @Test
+    @DisplayName("jdbc 대량 입력")
+    void insertBulk() {
+        long start = System.currentTimeMillis();
+        int size = 1000000;
+        List<Post> sampleData = createData(size);
+        bulkDataManager.saveBulk(sampleData, size);
+
+        System.out.println("대량 입력시간 : " + (System.currentTimeMillis() - start));
+    }
+}

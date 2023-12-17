b28e80d9347f73a2fc393e1579c617d5d135a588
kgpksh <tkdgns543@gmail.com>
2023-12-18 02:23:53 +0900

    feat: 개발, 테스트용 DB에 초기 데이터 자동 입력

---
+++ src/main/java/learning/practice/spring_forum_with_react/base/initData/TestData.java
@@ -0,0 +1,48 @@
+package learning.practice.spring_forum_with_react.base.initData;
+
+import learning.practice.spring_forum_with_react.base.testData.BulkDataManager;
+import learning.practice.spring_forum_with_react.boundedContext.post.entity.Post;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Profile({"dev", "test"})
+public class TestData {
+    private List<Post> createData(int size) {
+        String[] subjects = {"Cook", "Book", "Coding", "Humor"};
+        String author = "author";
+        String title = "title";
+        String content = "content";
+
+        List<Post> sampleData = new ArrayList<>(size);
+        for (int i = 1; i < size + 1; i++) {
+            Post post = new Post();
+            post.setAuthor(author + (i % 50));
+            post.setCategory((i % 4) + 1);
+            post.setTitle(title + i);
+            post.setContent(content + i);
+            sampleData.add(post);
+        }
+
+        return sampleData;
+    }
+
+    @Bean
+    CommandLineRunner initData(BulkDataManager bulkDataManager) {
+        return new CommandLineRunner() {
+            @Override
+            @Transactional
+            public void run(String... args) throws Exception {
+                int size = 630;
+                List<Post> sampleData = createData(size);
+                bulkDataManager.saveBulk(sampleData, size);
+            }
+        };
+    }
+}

ad0c79f10c38e01aeec07e832e6ce57a7cba35a7
kgpksh <tkdgns543@gmail.com>
2023-12-20 00:17:12 +0900

    feat: 캐시 도입

--- src/main/java/learning/practice/spring_forum_with_react/SpringForumWithReactApplication.java
+++ src/main/java/learning/practice/spring_forum_with_react/SpringForumWithReactApplication.java
@@ -2,10 +2,12 @@ package learning.practice.spring_forum_with_react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class SpringForumWithReactApplication {
 
 	public static void main(String[] args) {
